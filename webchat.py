0fdaac743d23debafd62e8b9d00ff4ca08afee3d
João Gehlen <joaogehlen91@gmail.com>
2016-08-11 17:30:33 -0300

    distribuindo as mensagens, mas  duplicando

--- webchat.py
+++ webchat.py
@@ -1,10 +1,21 @@
-from bottle import route, run, template, request, redirect, view, BaseResponse, BaseRequest, response, get
+from bottle import route, run, request, redirect, view, get
 import json
+import sys
+import time
+import requests
+import threading
 
 messages = []
+peers = sys.argv[2:]
 
-@get('/peers')
-def peers():
+
+@get('/get_peers')
+def get_peers():
+	return json.dumps(peers)
+
+
+@get('/get_messages')
+def get_messages():
 	return json.dumps(messages)
 
 
@@ -22,4 +33,37 @@ def send():
     redirect('/')
 
 
-run(host='localhost', port=8080)
+def sync_msgs():
+    time.sleep(6)
+    while True:
+        print('aqui')
+        time.sleep(2)
+        new_msgs = []
+        for p in peers:
+            r = requests.get(p + '/get_messages')
+            new_msgs = new_msgs + json.loads(r.text)
+
+        messages[:] = messages + new_msgs
+
+
+def sync_peers():
+    time.sleep(5)
+    while True:
+        np = []
+        time.sleep(5)
+        for p in peers:
+            r = requests.get(p + '/get_peers')
+            np = np + json.loads(r.text)
+        
+        peers[:] = list(set(np + peers))
+        print(peers)
+
+
+t = threading.Thread(target=sync_peers)
+t.start()
+t2 = threading.Thread(target=sync_msgs)
+t2.start()
+
+
+
+run(host='localhost', port=int(sys.argv[1]))
